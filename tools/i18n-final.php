0194c377cd61913fa3d7f8599403c715ac4f7160
Ben <hostellerie.org@gmail.com>
2026-08-29 12:53:19 +0200

    Fix final Videos i18n migration script

--- tools/i18n-final.php
+++ tools/i18n-final.php
@@ -1,10 +1,21 @@
 <?php
+
 $path = dirname(__DIR__) . '/public_html/index.php';
 $content = file_get_contents($path);
-$content = str_replace(
-    ") . '\">Afficher tout le catalogue</a></div>';",
-    ") . '\">' . htmlspecialchars($LANG_VIDEOS['catalogue_show_all'], ENT_QUOTES, 'UTF-8') . '</a></div>';",
-    $content
-);
-file_put_contents($path, $content);
+if ($content === false) {
+    throw new RuntimeException('Cannot read public_html/index.php');
+}
+
+$from = <<<'TXT'
+        ) . '">Afficher tout le catalogue</a></div>';
+TXT;
+$to = <<<'TXT'
+        ) . '">' . htmlspecialchars($LANG_VIDEOS['catalogue_show_all'], ENT_QUOTES, 'UTF-8') . '</a></div>';
+TXT;
+
+$content = str_replace($from, $to, $content);
+if (file_put_contents($path, $content) === false) {
+    throw new RuntimeException('Cannot write public_html/index.php');
+}
+
 echo "Final catalogue i18n fix applied.\n";
