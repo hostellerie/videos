06735c16325b183abf3bf2cab9199dd3749d5304
Ben <hostellerie.org@gmail.com>
2026-09-15 18:16:16 +0200

    Start Videos 0.20.0 development

--- version.php
+++ version.php
@@ -5,7 +5,7 @@ if (!isset($_CONF)) {
 }
 
 if (!defined('VIDEOS_PLUGIN_VERSION')) {
-    define('VIDEOS_PLUGIN_VERSION', '0.19.0');
+    define('VIDEOS_PLUGIN_VERSION', '0.20.0');
 }
 if (!defined('VIDEOS_MIN_GEEKLOG_VERSION')) {
     define('VIDEOS_MIN_GEEKLOG_VERSION', '2.1.1');
@@ -14,7 +14,7 @@ if (!defined('VIDEOS_MIN_PHP_VERSION')) {
     define('VIDEOS_MIN_PHP_VERSION', '5.6.0');
 }
 if (!defined('VIDEOS_RELEASE_STATUS')) {
-    define('VIDEOS_RELEASE_STATUS', 'stable');
+    define('VIDEOS_RELEASE_STATUS', 'development');
 }
 
 require_once __DIR__ . '/interoperability.php';
